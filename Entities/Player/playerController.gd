cd339b10c17e1ceea0be44f72b2e95a49a5008a2
Jordi <jorrom16@gmail.com>
2026-04-29 14:54:05 +0100

    Updated Player Controller: Added Momentum
    - Added momentum for horizontal movement
    - Changed how the ledge jump works, where the player now jumps straight up instead of first teleporting to the direction of the ledge on the X axis before jumping
    - Added a ledge grab timer which prevents the player from instantly regrabbing onto the ledge if they drop down while holding in the direction of the ledge
    - Added a minimum jump velocity variable which allows the transition from jumping to falling a bit more "floaty" rather than instant
    
    Notes:
    - Variable refinement required for the momentum
    - Need more experimenting with the ledge grab and climb

--- Entities/Player/playerController.gd
+++ Entities/Player/playerController.gd
@@ -1,18 +1,26 @@
 extends CharacterBody3D
 
-enum STATE { FALL, FLOOR, JUMP, LEDGE_GRAB, LEDGE_HANG, LEDGE_JUMP }
+enum STATE { FALL, FLOOR, JUMP, LEDGE_CLIMB, LEDGE_HANG, LEDGE_JUMP }
 
 #Player Physics variables
+
+# # Vertical Physics
 @export var FALL_GRAVITY := 30.0
 @export var FALL_VELOCITY := -20.0
+@export var JUMP_VELOCITY := 15.0
+var MIN_JUMP_VELOCITY := 5.0
+@export var JUMP_DECELERATION := 35.0
+@export var LEDGE_JUMP_VELOCITY := 17.0
+
+# # Horizontal Physics
 @export var WALK_VELOCITY := 10.0
-@export var JUMP_VELOCITY := 20.0
-@export var JUMP_DECELERATION := 30.0
-@export var LEDGE_JUMP_VELOCITY := 20.0
+@export var WALK_ACCEL := 20.0
+@export var FRICTION := 15.0
 
 #Misc variables
 @onready var player_sprite: AnimatedSprite3D = %PlayerSprite
 @onready var coyote_timer: Timer = %CoyoteTimer
+@onready var ledge_grab_timer: Timer = %LedgeGrabTimer
 @onready var state_debug: Label3D = %StateDebug
 
 #Ledge grab variables
@@ -27,7 +35,7 @@ var facingDirection := 1.0
 
 func _ready() -> void:
 	SwitchState(activeState)
-	rc_ledge_grab.add_exception(self)
+	rc_ledge_grab.add_exception(self) #prevents the raycast from detecting the player's hitbox
 
 func _physics_process(delta: float) -> void:
 	ProcessState(delta)
@@ -46,11 +54,12 @@ func SwitchState(toState: STATE) -> void:
 				coyote_timer.start()
 			
 		STATE.JUMP:
+			MIN_JUMP_VELOCITY = 5.0 #sets the min jump when on ground
 			player_sprite.play("jump")
 			velocity.y = JUMP_VELOCITY
 			coyote_timer.stop()
 		
-		STATE.LEDGE_GRAB:
+		STATE.LEDGE_CLIMB:
 			player_sprite.play("ledgeClimb")
 			velocity = Vector3.ZERO
 			global_position.y = rc_ledge_grab.get_collision_point().y - (player_collider.shape.height * 0.5)
@@ -62,8 +71,9 @@ func SwitchState(toState: STATE) -> void:
 			global_position.y = rc_ledge_grab.get_collision_point().y - (player_collider.shape.height * 0.5)
 		
 		STATE.LEDGE_JUMP:
-			player_sprite.play("jump") #this should be a different animation from the regular jump
-			velocity.y = LEDGE_JUMP_VELOCITY
+			MIN_JUMP_VELOCITY = 7.5 #sets the min jump when entering a ledge jump
+			player_sprite.play("jump") #this should be a different animation from the regular jump #should it??
+			velocity.y = JUMP_VELOCITY
 	
 	state_debug.text = str(STATE.keys()[activeState])
 
@@ -71,23 +81,24 @@ func ProcessState(delta: float) -> void:
 	match activeState:
 		STATE.FALL:
 			velocity.y = move_toward(velocity.y, FALL_VELOCITY, FALL_GRAVITY * delta)
-			HandleMovement()
+			HandleMovement(delta)
 			
 			if is_on_floor():
 				SwitchState(STATE.FLOOR)
 			elif Input.is_action_just_pressed("jump") and coyote_timer.time_left > 0:
 				SwitchState(STATE.JUMP)
-			elif IsInputTowardFacing() and IsLedge() and IsSpace():
-				SwitchState(STATE.LEDGE_GRAB)
-			elif IsInputTowardFacing() and IsLedge() and not IsSpace():
-				SwitchState(STATE.LEDGE_HANG)
+			elif ledge_grab_timer.time_left == 0.0 and IsInputTowardFacing() and IsLedge():
+				if IsSpace():
+					SwitchState(STATE.LEDGE_CLIMB)
+				elif not IsSpace():
+					SwitchState(STATE.LEDGE_HANG)
 		
 		STATE.FLOOR:
 			if Input.get_axis("moveLeft", "moveRight"):
 				player_sprite.play("walk")
 			else:
 				player_sprite.play("idle")
-			HandleMovement()
+			HandleMovement(delta)
 			
 			if not is_on_floor():
 				SwitchState(STATE.FALL)
@@ -96,45 +107,60 @@ func ProcessState(delta: float) -> void:
 		
 		STATE.JUMP, STATE.LEDGE_JUMP:
 			velocity.y = move_toward(velocity.y, 0, JUMP_DECELERATION * delta)
-			HandleMovement()
+			HandleMovement(delta)
 			
 			if Input.is_action_just_released("jump") or velocity.y <= 0:
-				velocity.y = 0
+				velocity.y = min(velocity.y, MIN_JUMP_VELOCITY)
 				SwitchState(STATE.FALL)
 		
-		STATE.LEDGE_GRAB:
+		STATE.LEDGE_CLIMB:
 			if not player_sprite.is_playing():
 				var offset := LedgeClimbOffset()
 				offset.x *= facingDirection
 				position += offset
 				SwitchState(STATE.FLOOR)
 			elif Input.is_action_just_pressed("jump"):
-				#allows the player to jump out of the ledge climb at any point
-				var progress := inverse_lerp(0, player_sprite.sprite_frames.get_frame_count("ledgeClimb"), player_sprite.frame)
-				var offset := LedgeClimbOffset()
-				offset.x *= facingDirection * progress
-				position += offset
 				SwitchState(STATE.LEDGE_JUMP)
 		
 		STATE.LEDGE_HANG:
 			if Input.is_action_just_pressed("jump"):
 				SwitchState(STATE.LEDGE_JUMP)
+			#lets the player drop down
 			elif Input.is_action_just_pressed("down"):
+				ledge_grab_timer.start()
 				SwitchState(STATE.FALL)
 
-func HandleMovement() -> void:
+func HandleMovement(delta: float) -> void:
 	var inputDirection := signf(Input.get_axis("moveLeft", "moveRight"))
+	
+	#Old horizontal movement code
+	#if inputDirection:
+		#player_sprite.flip_h = inputDirection < 0
+		#facingDirection = inputDirection
+		#
+		#Mirrors the raycast direction based on the input direction
+		#rc_ledge_grab.position.x = facingDirection * absf(rc_ledge_grab.position.x)
+		#rc_ledge_grab.target_position.x = facingDirection * absf(rc_ledge_grab.target_position.x)
+		#rc_ledge_grab.force_raycast_update()
+	
+	#basic horizontal movement, need to implement proper momentum
+	#velocity.x = inputDirection * WALK_VELOCITY
+	
+	#New horizontal movement code with momentum calculation
 	if inputDirection:
 		player_sprite.flip_h = inputDirection < 0
 		facingDirection = inputDirection
 		
 		#Mirrors the raycast direction based on the input direction
-		rc_ledge_grab.position.x = inputDirection * absf(rc_ledge_grab.position.x)
-		rc_ledge_grab.target_position.x = inputDirection * absf(rc_ledge_grab.target_position.x)
+		rc_ledge_grab.position.x = facingDirection * absf(rc_ledge_grab.position.x)
+		rc_ledge_grab.target_position.x = facingDirection * absf(rc_ledge_grab.target_position.x)
 		rc_ledge_grab.force_raycast_update()
-	
-	#basic horizontal movement, need to implement proper momentum
-	velocity.x = inputDirection * WALK_VELOCITY
+		
+		var targetSpeed = inputDirection * WALK_VELOCITY
+		velocity.x = move_toward(velocity.x, targetSpeed, WALK_ACCEL * delta)
+	else:
+		#Apply friction when no input is detected
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
 #Prevents ledge grab if no input is detected when passing a ledge
 func IsInputTowardFacing() -> bool:
@@ -147,6 +173,8 @@ func IsLedge() -> bool:
 	not rc_head_check.is_colliding() and \
 	rc_ledge_grab.get_collision_normal().is_equal_approx(Vector3.UP)
 
+#Checks if there is space for the player to climb up
+#Determined by the height of the Ledge Space Raycast, which is the height of the player's hitbox
 func IsSpace() -> bool:
 	rc_ledge_space.global_position = rc_ledge_grab.get_collision_point()
 	rc_ledge_space.force_raycast_update()
